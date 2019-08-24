98b41d6d9137b79b6d57de7cb015a9e966a7a8f8
Wei Luo <luoweihoo@yahoo.com>
2019-08-24 14:49:31 +0800

    readpi commit

---
+++ Chapter10/ReadPi.py
@@ -0,0 +1,11 @@
+# AUTHOR: Wei Luo
+# FILE: C:\Users\Luo Wei\OneDrive\Python\Python-Crash-Course\ReadPi.py
+# Contact: luoweihoo@yahoo.com
+# DATE: 2019/08/24 Sat
+# TIME: 14:45:44
+
+with open('pi_digits.txt') as file_object:
+    contents = file_object.read()
+    print(contents.rstrip())
+
+    
