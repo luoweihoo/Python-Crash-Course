6609ecde96c28457189c12b5134d9cfa2c19c953
Wei Luo <luoweihoo@yahoo.com>
2019-09-01 01:28:13 +0800

    update code

---
+++ Chapter12/settings.py
@@ -0,0 +1,16 @@
+# @File    :   settings.py
+# @Time    :   2019/08/30 22:11:31
+# @Author  :   Wei Luo 
+# @Version :   1.0
+# @Contact :   luoweihoo@yahoo.com
+# @Desc    :   None
+
+class Settings:
+    # A class to store all settings for Alien Invations
+    def __init__(self):
+        # Initialize the game's settings
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
---
+++ Chapter12/ship.py
@@ -0,0 +1,26 @@
+# @File    :   ship.py
+# @Time    :   2019/08/30 22:26:28
+# @Author  :   Wei Luo 
+# @Version :   1.0
+# @Contact :   luoweihoo@yahoo.com
+# @Desc    :   None
+
+import pygame
+
+class Ship:
+    # A class to manage the ship
+    def __init__(self, ai_game):
+        # Initialize the ship and set its starting postion
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the ship image and get its rect
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom center of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        # Draw the ship at its current location
+        self.screen.blit(self.image, self.rect)
